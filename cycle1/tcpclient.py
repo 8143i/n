49e51077d4ff5e5c8d49967c013615a5e9cb7f7e
8143i <pubggamers5038@gmail.com>
2025-11-10 12:21:37 +0530

    Update tcpclient.py

--- cycle1/tcpclient.py
+++ cycle1/tcpclient.py
@@ -1,18 +1,19 @@
 import socket
 
-s = socket.socket()                 # Step1: Create client socket
+s = socket.socket()               
 s.connect(('localhost', 12345))
 print("Connected to server. Type 'bye' to exit.\n")
 
 while True:
-    msg = input("Client: ")         # Step3: Get message from client user
-    s.send(msg.encode())            # Step4: Send message to server
+    msg = input("Client: ")       
+    s.send(msg.encode())          
     if msg.lower() == 'bye':
         break
 
-    reply = s.recv(1024).decode()   # Step2: Receive server message
+    reply = s.recv(1024).decode() 
     print("Server:", reply)
     if reply.lower() == 'bye':
         break
 
 s.close()
+
